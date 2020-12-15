3002bdd2ef052f064cdc7ce158d51763d891d232
Yuliforms <76013984+Yuliforms@users.noreply.github.com>
2020-12-15 01:21:45 -0600

    Create enviar.js

---
+++ cmp/enviar.js
@@ -0,0 +1,8 @@
+function enviar() {
+         const bolet = boleta.value;
+         const name = nombre.value;
+         const group = grupo.value;
+         const materi = materia.value;
+         const date = fecha.value;
+         
+        }
