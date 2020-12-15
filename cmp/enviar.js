a61aad1248596eae3b538f1c0d7b003b55a5571d
Yuliforms <76013984+Yuliforms@users.noreply.github.com>
2020-12-15 14:41:09 -0600

    Update enviar.js

--- cmp/enviar.js
+++ cmp/enviar.js
@@ -1,8 +1,14 @@
-function enviar() {
-         const bolet = boleta.value;
-         const name = nombre.value;
-         const group = grupo.value;
-         const materi = materia.value;
-         const date = fecha.value;
-         
-        }
+var formu = document.getElementById("formulario"),
+    salida = document.getElementById("salida");
+    
+formu.addEventListener("submit", enviar, false);
+function enviar(){
+  var boleta = formu["boleta"].value,  
+      nombre = formu["nombre"].value,  
+      grupo = formu["grupo"].value,  
+      materia = formu["materia"].value,  
+      fecha = new Date(formu["fecha"].value),
+      miFecha;
+      miFecha = fecha.getDate() + 1 + "/"+ (fecha.getMonth() + 1) +"/"+fecha.getFullYear();
+    salida.textContent="Boleta: " + boleta + " Nombre: "+ nombre+ " Grupo: "+grupo + "Materia: "+materia+ "Fecha: "+miFecha;
+}
