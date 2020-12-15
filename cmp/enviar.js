422620e88b44942df597c6637b50e175b6f03fbc
Yuliforms <76013984+Yuliforms@users.noreply.github.com>
2020-12-15 01:26:40 -0600

    Update enviar.js

--- cmp/enviar.js
+++ cmp/enviar.js
@@ -7,8 +7,8 @@ var forma = document.getElementById("forma"),
 
  
 
-forma.addEventListener("submit", procesa, false);
-function procesa(){
+forma.addEventListener("submit", enviar, false);
+function enviar(){
     for (var i = 0, longitud=campos.length; i < longitud; i++) {
       var campo = forma[campos[i]];
       var salida = document.getElementById(salidas[i]);
